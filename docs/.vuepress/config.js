df72ab5925432c4e8a5957a66afe0a8865646f00
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-11 17:23:03 -0400

    remove html link plugin for now to see if it crawls better

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,24 @@
 module.exports = {
   title: 'Cypress examples',
-  description: 'Static site with Cypress examples tested right from the Markdown sources',
+  description:
+    'Static site with Cypress examples tested right from the Markdown sources',
   base: '/cypress-examples/',
   plugins: [
-    [
-      'vuepress-plugin-clean-urls',
-      {
-        normalSuffix: '/',
-        indexSuffix: '/',
-        notFoundPath: '/404.html',
-      },
-    ],
+    // [
+    //   'vuepress-plugin-clean-urls',
+    //   {
+    //     normalSuffix: '/',
+    //     indexSuffix: '/',
+    //     notFoundPath: '/404.html',
+    //   },
+    // ],
   ],
+  themeConfig: {
+    algolia: {
+      // DANGER 🧨💀: ONLY USE ALGOLIA PUBLIC SEARCH-ONLY API KEY
+      apiKey: 'd2cc2084df39806bdefb04f60f16e856',
+      indexName: 'cypress-examples',
+      appId: '48DTXR75RW',
+    },
+  },
 }
