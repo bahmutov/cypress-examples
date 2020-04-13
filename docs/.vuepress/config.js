81b1377ce126edfa1f26ebb6a11d6832fe3d01d3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-13 17:01:49 -0400

    add edit this page on GH link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,11 @@ module.exports = {
   base: '/cypress-examples/',
   plugins: [],
   themeConfig: {
+    // point pages back at the GitHub documents
+    repo: 'bahmutov/cypress-examples',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
     nav: [
       {
         text: 'Commands',
@@ -31,14 +36,15 @@ module.exports = {
   },
   markdown: {
     extendMarkdown: (md) => {
-      // console.log(md)
       if (!_highlight) {
         _highlight = md.options.highlight
       }
 
+      // every time we see HTML code block
+      // it is part of Cypress mini test
+      // so we really want to show the HTML source
+      // AND mount it as a live example block to run tests against
       md.options.highlight = (str, lang) => {
-        // return _highlight(str, lang)
-
         if (lang !== 'html') {
           return _highlight(str, lang)
         }
