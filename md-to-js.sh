438a7cab2cff150bb37326d90f2e81b6c7ab04aa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-22 08:08:04 -0400

    move MD to JS conversion into bash script

---
+++ md-to-js.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# converts Markdown fiddle specs into JavaScript specs
+BASE_URL=$1
+if [ -z $BASE_URL ]
+then
+  echo "Provide base url, please"
+  script=`basename "$0"`
+  echo "For example: $script http://localhost:5000/cypress-examples"
+  exit 1
+fi
+
+echo "Each spec file will visit $BASE_URL before tests in each spec"
+npx export-fiddle docs/commands/actions.md --before $BASE_URL/commands/actions
+npx export-fiddle docs/commands/connectors.md --before $BASE_URL/commands/connectors
+npx export-fiddle docs/commands/querying.md --before $BASE_URL/commands/querying
+npx export-fiddle docs/commands/traversal.md --before $BASE_URL/commands/traversal
+ls -laR docs/**/*.js
