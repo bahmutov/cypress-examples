4f5d7703e6b95a30260ce886684b13df57699a97
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-23 18:39:44 -0400

    fix: add full minified jquery

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,9 +22,9 @@ module.exports = {
     [
       'script',
       {
-        src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js',
+        src: 'https://code.jquery.com/jquery-3.5.0.min.js',
         integrity:
-          'sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=',
+          'sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=',
         crossorigin: 'anonymous',
       },
     ],
