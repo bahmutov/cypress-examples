9dd3cd220f008fad0377bf525cf488649df3cc3f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-26 09:45:52 -0400

    switch to shelljs for markdown to js conversion

---
+++ md-to-js.js
@@ -0,0 +1,38 @@
+const shell = require('shelljs')
+const path = require('path')
+
+// converts Markdown fiddle specs into JavaScript specs
+let baseUrl = process.argv[2]
+if (!baseUrl) {
+  const DEFAULT_BASE_URL = 'http://localhost:5000/cypress-examples'
+  console.log('Setting empty baseUrl to %s', DEFAULT_BASE_URL)
+  baseUrl = DEFAULT_BASE_URL
+}
+
+console.log(
+  'Each spec file will visit %s before tests in each spec',
+  baseUrl,
+)
+
+const rootFolder = 'docs'
+const markdownFiles = shell.ls(`${rootFolder}/**/*.md`)
+console.log('Markdown files\n' + markdownFiles.join('\n'))
+
+markdownFiles.forEach((filename) => {
+  const inRoot = path.relative(rootFolder, filename)
+  const pagePath = inRoot.replace(/\.md$/, '')
+  // navigation spec is a little different - it will navigate to its page itself
+  const beforeVisitUrl = filename.endsWith('navigation.md')
+    ? baseUrl
+    : `${baseUrl}/pagePath`
+  shell.exec(
+    `npx export-fiddle ${filename} --before ${beforeVisitUrl}`,
+  )
+})
+
+shell.exec('ls -lAR docs/**/*.js')
+console.log(
+  'You can run these specs against %s with command',
+  baseUrl,
+)
+console.log('npx cypress open --config-file cypress-dist.json')
