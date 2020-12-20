3cb8bcb990e32fcd43ea7d63d69974961d4acfa9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-12-20 15:37:39 -0500

    fix: use webpack preprocessor dependency

--- plugins.js
+++ plugins.js
@@ -1,4 +1,4 @@
-const mdPreprocessor = require('@cypress/fiddle/src/markdown-preprocessor')
+const mdPreprocessor = require('cypress-markdown-preprocessor')
 module.exports = (on, config) => {
   on('file:preprocessor', mdPreprocessor)
   return config
