e80d1aaa36c71ef28359c665174afa9239831470
bahmutov <bahmutov@users.noreply.github.com>
2023-03-30 02:40:10 +0000

    deploy: a7776af9ea802d997ff8e50aa8c61fd4cb5d649e

---
+++ assets/second-text-match.html-e7c436d4.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-8a3b7544","path":"/recipes/second-text-match.html","title":"Second Text Match","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1680143518000,"contributors":[{"name":"Gleb Bahmutov","email":"gleb.bahmutov@gmail.com","commits":1}]},"filePathRelative":"recipes/second-text-match.md"}');export{e as data};
