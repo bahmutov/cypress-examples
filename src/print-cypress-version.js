b41f4b26c3875fa50ad697afee5686efc1a54e29
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-04 07:37:30 -0400

    replace set-output command, closes #147

--- src/print-cypress-version.js
+++ src/print-cypress-version.js
@@ -2,7 +2,8 @@
 const { findCypressVersion } = require('./utils')
 // prints found Cypress dependency version without newline
 // to make it convenient to set the Cypress version
-// in the workflow using ::set-output command
+// in the workflow using set output command
+// https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
 // https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
 // https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions#outputs
 //
