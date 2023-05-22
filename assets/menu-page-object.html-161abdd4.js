9316c32128610409fcd9f950ab73a6675de528a6
bahmutov <bahmutov@users.noreply.github.com>
2023-05-22 22:48:02 +0000

    deploy: 18c1007a1fde93c59db0d8d59e74b1c4781657d1

---
+++ assets/menu-page-object.html-161abdd4.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-735dff27","path":"/recipes/menu-page-object.html","title":"Menu Page Object","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1684795298000,"contributors":[{"name":"Gleb Bahmutov","email":"gleb.bahmutov@gmail.com","commits":1}]},"filePathRelative":"recipes/menu-page-object.md"}');export{e as data};
