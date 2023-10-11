09c8b1572e5271f4bf984bb6bedf26fb4735ae55
bahmutov <bahmutov@users.noreply.github.com>
2023-10-11 16:03:04 +0000

    deploy: e2c54626ae8f69fe6258e2210ffe59431d608b36

---
+++ assets/last-of-type.html-2ef9ecca.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-aec61906","path":"/recipes/last-of-type.html","title":"Last of type CSS selector","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1697039328000,"contributors":[{"name":"Gleb Bahmutov","email":"gleb.bahmutov@gmail.com","commits":1}]},"filePathRelative":"recipes/last-of-type.md"}');export{e as data};
