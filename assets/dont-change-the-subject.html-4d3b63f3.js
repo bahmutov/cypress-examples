d2a91ab621f096f290846defe215b28d4c63d3d2
bahmutov <bahmutov@users.noreply.github.com>
2025-02-08 00:24:09 +0000

    deploy: e9c0b68afae1037d2ef07c2932634eeca0c37d59

---
+++ assets/dont-change-the-subject.html-4d3b63f3.js
@@ -0,0 +1,1 @@
+const e=JSON.parse(`{"key":"v-6e5bb0ed","path":"/recipes/dont-change-the-subject.html","title":"Don't Change The Subject","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1738973918000,"contributors":[{"name":"Gleb Bahmutov","email":"gleb.bahmutov@gmail.com","commits":1}]},"filePathRelative":"recipes/dont-change-the-subject.md"}`);export{e as data};
