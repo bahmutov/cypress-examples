e8b5a5ff7f6af36a94c03d1893080acd044478ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-08-11 15:53:30 -0400

    print after results

--- plugins.js
+++ plugins.js
@@ -5,6 +5,18 @@ const {
 
 module.exports = (on, config) => {
   on('file:preprocessor', mdPreprocessor)
+
+  on('after:spec', (spec, results) => {
+    console.log('after:spec')
+    console.log(spec)
+    console.log(results)
+  })
+
+  on('after:run', (results) => {
+    console.log('after:run')
+    console.log(results)
+  })
+
   // https://github.com/bahmutov/cypress-book/issues/82
   // registerBookScreenshots(on, config)
   return config
