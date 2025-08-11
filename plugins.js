392491d42b91eff86701f28b13c30db2da57275b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-08-11 16:06:50 -0400

    print only the relevant information

--- plugins.js
+++ plugins.js
@@ -7,19 +7,39 @@ module.exports = (on, config) => {
   on('file:preprocessor', mdPreprocessor)
 
   on('before:run', (details) => {
-    console.log('before:run')
-    console.log(details)
+    const { group, parallel, runUrl } = details
+    if (runUrl) {
+      console.log('Run URL %s', runUrl)
+      if (parallel) {
+        console.log('Running specs in parallel')
+      }
+      if (group) {
+        console.log('Group %s', group)
+      }
+    }
   })
 
   on('after:spec', (spec, results) => {
-    console.log('after:spec')
-    console.log(spec)
-    console.log(results)
+    const { relative } = spec
+    const failures = results.stats?.failures
+    if (failures) {
+      console.log('Spec %s had %d failures', relative, failures)
+      // look into results.tests objects
+      // attempts, displayError, duration, state, title (array of strings)
+    }
   })
 
   on('after:run', (results) => {
+    const { runUrl, totalTests, totalFailed } = results
     console.log('after:run')
-    console.log(results)
+    console.log(
+      '%d total tests, %d failed tests',
+      totalTests,
+      totalFailed,
+    )
+    if (runUrl) {
+      console.log('Run URL %s', runUrl)
+    }
   })
 
   // https://github.com/bahmutov/cypress-book/issues/82
